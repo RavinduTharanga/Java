8442414f9f4381067d5442808f183e3f2b3bb3ae
RavinduTharanga <ravindutharangaperera@gmail.com>
2026-04-23 22:07:03 -0400

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,78 +1,138 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
-    static Scanner scanner =new Scanner(System.in);
 
     public static void main(String[] args){
 
+        Scanner scanner = new Scanner(System.in);
+
+
+        //Java slot machine
         //Declare variables
+        int balance=100;
+        int bet;
+        int payout;
+        String[] row;
+        String playAgain;
+        //Display welcome message
+        System.out.println("*************************");
+        System.out.println("  Welcome to Java Slots  ");
+        System.out.println("Symbols: 🍒 🍉 🍋 🔔 ⭐");
+        System.out.println("*************************");
 
-        double balance=0;
-        boolean isRunning=true;
-        int choice;
 
-        while (isRunning) {
-            System.out.println("*****************");
-            System.out.println(" BANKING PROGRAM");
-            System.out.println("*****************");
-            System.out.println("1. Show Balance");
-            System.out.println("2. Deposit");
-            System.out.println("3. Withdraw");
-            System.out.println("4. Exit");
-            System.out.println("*****************");
+        //Play if balance>0
 
-            System.out.print("Enter your choice (1-4):");
-            choice = scanner.nextInt();
-
-            switch (choice) {
-                case 1 -> showBalance(balance);
-                case 2 -> balance=balance+deposit();
-                case 3 -> balance=balance-withdraw(balance);
-                case 4 -> isRunning=false;
-                default -> System.out.println("Invalid choice");
+        while(balance>0){
+            System.out.println("Your current balance: $"+ balance);
+            System.out.print("Place your bet amount: ");
+            bet=scanner.nextInt();
+            scanner.nextLine();
+            if (bet> balance){
+                System.out.println("Insufficient funds");
+                continue;
             }
+            else if(bet<=0){
+                System.out.println("Bet must be greater than 0");
+                continue;
+            }
+            else{
+                balance=balance-bet;
+
+            }
+            System.out.println("Spinning.....");
+            row=spinRow();
+            printRow(row);
+            payout=getPayout(row,bet);
+            if (payout>0){
+                System.out.println("You Won $"+payout);
+                balance=balance+payout+bet;
+            }
+            else {
+                System.out.println("Sorry you lost this round");
+            }
+            System.out.println("Do you want to play Again? (Y/N)");
+            playAgain=scanner.nextLine().toUpperCase();
+            if(!playAgain.equals("Y")){
+                break;
+            }
+
+
+
         }
-        //Display menu
-        //Get and process user input
-        //Show balance
-        //deposit
-        //Withdraw
-        //Exit Message
-        System.out.println("*****************");
-        System.out.println("Thank you have a nice day");
+        System.out.println("Game Over..Your final balance is $"+balance);
+        //Enter Bet Amount
+        // Verify if bet > balance
+        //Verify if bet > 0
+        //Subtract bet from balance
+        //Spin row
+        //Print row
+        //Get Payout
+        //Ask to play again
+        //Display exit message
+
         scanner.close();
-    }
-    static void showBalance(double balance){
-        System.out.println("*****************");
-        System.out.printf("$%.2f\n",balance);
 
     }
-    static double deposit(){
 
-        double amount;
-        System.out.println("Enter amount to deposited: ");
-        amount= scanner.nextDouble();
-        if (amount<0){
-            System.out.println("Amount can't be negative");
-        }
-        else {
-            return amount;
+    //Spinrow
+    static String[] spinRow(){
+        String[] symbols ={"🍒","🍉","🍋","🔔","⭐"};
+        String[] row = new String[3];
+        Random random = new Random();
+
+        for(int i=0; i<3;i++){
+
+            row[i]=symbols[random.nextInt(symbols.length)];
 
         }
-        return amount;
+        return row;
     }
-    static double withdraw(double balance){
-        double amount;
-        System.out.println("Enter amount to be withdraw: ");
-        amount = scanner.nextDouble();
-        if (amount>balance){
-            System.out.println("Not enough money to withdraw");
-            return 0;
-        }
-        else {
-            return amount;
-        }
+    static void printRow(String[] row){
+        System.out.println("*************************");
+        System.out.println(" "+ String.join(" | ",row));
+        System.out.println("*************************");
 
+
+    }
+    static int getPayout(String[]row,int bet){
+        if(row[0].equals(row[1])&& row[1].equals(row[2])){
+            return switch (row[0]){
+                case "🍒"->bet*3;
+                case "🍉"->bet*4;
+                case "🍋"->bet*5;
+                case "🔔"->bet*10;
+                case "⭐"->bet*20;
+                default -> 0;
+
+            };
+
+
+        }
+        else if(row[0].equals(row[1])) {
+            return switch (row[0]) {
+                case "🍒" -> bet * 2;
+                case "🍉" -> bet * 3;
+                case "🍋" -> bet * 4;
+                case "🔔" -> bet * 5;
+                case "⭐" -> bet * 10;
+                default -> 0;
+
+            };
+        }
+        else if(row[1].equals(row[2])) {
+            return switch (row[1]) {
+                case "🍒" -> bet * 2;
+                case "🍉" -> bet * 3;
+                case "🍋" -> bet * 4;
+                case "🔔" -> bet * 5;
+                case "⭐" -> bet * 10;
+                default -> 0;
+
+            };
+        }
+        return 0;
     }
 }
